35c81cb4a9af775550d8dd08f4373d5c7af83aea
Christian <christiangeorge602@gmail.com>
2025-11-16 15:46:59 -0500

    updated functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 var burgerMenu = document.querySelector(".burger-menu");
 var bars = document.querySelectorAll(".bar");
 var navLinks = document.querySelector(".nav-links");
-
+var ellipsesMenu = document.querySelector(".ellipses-menu")
 var container = document.querySelector(".container")
-
+const dropdown = document.querySelector('.ellipses-dropdown');
 
 
 burgerMenu.addEventListener("click", () => {
@@ -12,3 +12,27 @@ burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("active");
 
 })
+
+document.addEventListener("click", (e)=> {
+  if (!burgerMenu.contains(e.target) && !navLinks.contains(e.target))  {
+    burgerMenu.classList.remove("active");
+    navLinks.classList.remove("showNav");
+    container.classList.remove("hideContent")
+  }
+})
+
+ellipsesMenu.addEventListener("click", ()=> {
+dropdown.classList.toggle("active")
+
+})
+
+document.addEventListener('click', (e) => {
+  console.log(e.target);
+  
+  if (!dropdown.contains(e.target) && !ellipsesMenu.contains(e.target)) {
+    dropdown.classList.remove('active');
+  }
+});
+
+
+
